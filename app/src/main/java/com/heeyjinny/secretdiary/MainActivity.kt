507abee4672382a5c98fceb6e00caaf2b4de4c20
heeyjinny <heeyjinny@gmail.com>
2023-01-03 22:08:57 +0900

    viewBinding

--- app/src/main/java/com/heeyjinny/secretdiary/MainActivity.kt
+++ app/src/main/java/com/heeyjinny/secretdiary/MainActivity.kt
@@ -2,10 +2,15 @@ package com.heeyjinny.secretdiary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.heeyjinny.secretdiary.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //뷰바인딩
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
